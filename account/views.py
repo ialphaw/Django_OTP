0e81aca9f021af126f3f1495107af9b8d282509d
ialphaw <amirabbas.vaziri@gmail.com>
2020-12-01 22:30:08 +0330

    create views for 'account' app

--- account/views.py
+++ account/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework.generics import RetrieveAPIView
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from account.permissions import IsPassenger, IsDriver
+from account.serializers import DriverSerializer, PassengerSerializer
+
+
+class DriverInfo(RetrieveAPIView):
+    permission_classes = [IsAuthenticated, IsDriver]
+    serializer_class = DriverSerializer
+
+    def get_object(self):
+        return self.request.user.driver
+
+
+class PassengerInfo(RetrieveAPIView):
+    permission_classes = [IsAuthenticated, IsPassenger]
+    serializer_class = PassengerSerializer
+
+    def get_object(self):
+        return self.request.user.passenger
