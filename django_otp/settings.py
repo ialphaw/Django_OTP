ce52c1b0def0758ce1bbdaf6c235c4bae3cbf79a
ialphaw <amirabbas.vaziri@gmail.com>
2020-12-01 13:29:52 +0330

    install rest framework

--- django_otp/settings.py
+++ django_otp/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Rest-Framework
+    'rest_framework',
+
+    # Local Apps
+    
 ]
 
 MIDDLEWARE = [
