c33444b4ed8d505d1495c8d6bdc4737265486487
ialphaw <amirabbas.vaziri@gmail.com>
2020-12-02 01:03:48 +0330

    add AuthUser model to 'authentication' app

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class AuthUser(models.Model):
+    phone = models.CharField(max_length=11)
+    auth_code = models.CharField(max_length=6)
+
+    def __str__(self):
+        return self.auth_code
