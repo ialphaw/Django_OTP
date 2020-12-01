5067877753aafcd414dfd66a2eaf7309f88b445f
ialphaw <amirabbas.vaziri@gmail.com>
2020-12-01 13:36:35 +0330

    add 'Driver' & 'Passenger' in 'account' app

--- account/models.py
+++ account/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
-# Create your models here.
+
+class Driver(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    national_code = models.CharField(max_length=10,
+                                     validators=[MinLengthValidator(10)],
+                                     unique=True)
+    phone = models.CharField(max_length=11)
+
+    def __str__(self):
+        return self.user.get_full_name()
+
+
+class Passenger(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    email = models.EmailField(max_length=50)
+    phone = models.CharField(max_length=11)
+
+    def __str__(self):
+        return self.user.get_full_name()                      
